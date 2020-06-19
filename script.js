f4c2ad42cdc5a05d949034dfa81418fa68f72734
aayush-grover <aayushgrover3@gmail.com>
2020-06-19 23:55:30 +0530

    lets glorify Coronavirus text

--- script.js
+++ script.js
@@ -7,12 +7,9 @@ if(element.hasChildNodes()){
 else if(element.nodeType === Text.TEXT_NODE){
     if(element.textContent.match(/coronavirus/gi))
     {
-    //     const replacedText = document.createElement('span')
-    //     replacedText.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span style="background-color: black;color: black;">$1</span>')
-    //     element.replaceWith(replacedText)
-    // }
-    // element.textContent = element.textContent.replace(/coronavirus/gi, 'Stay Home')
-    element.parentElement.remove()
+        const replacedText = document.createElement('span')
+        replacedText.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>')
+        element.replaceWith(replacedText)
     }
 }
 }
